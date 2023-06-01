5e800601d66e1b16036274760f5913ec92a21a92
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-06-01 23:50:53 +0500

    Completed

---
+++ Assignment02/app.js
@@ -0,0 +1,27 @@
+//------------------- Q 01 ----------------------
+    // var username;
+//------------------- Q 02 ----------------------
+    // var myName = "Muhammad Kamil Raza";
+//------------------- Q 03 ----------------------
+    // var message = "Hello World"
+    // alert(message);
+//------------------- Q 04 ----------------------
+    // var sName = "John Doe";
+    // var age = "19 years old";
+    // var certify = "Certified MERN Stack Developer"
+    // alert(sName);
+    // alert(age);
+    // alert(certify);
+//------------------- Q 05 ----------------------
+    // alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+//------------------- Q 06 ----------------------
+    // var email = "m.kamilrazabhatti@gmail.com";
+    // alert("My email address is " + email    );
+//------------------- Q 07 ----------------------
+    // var book = "A smarter way to learn JavaScript";
+    // alert("I am trying to learn from the book " + book);
+//------------------- Q 08 ----------------------
+    // document.write("Yah! i can write HTML content through JavaScript.");
+//------------------- Q 09 ----------------------
+    // var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+    // alert(design);
